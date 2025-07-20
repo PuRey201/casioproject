cfc3a57f1c0cf12b9d13ea9fd37ec80f08332745
PuRey201 <sereykeam18@gmail.com>
2025-07-20 17:38:30 +0700

    Fix: Add proper start command to Dockerfile

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    preview: {
-        allowedHosts: ['casioproject.onrender.com'],
+  server: {
+    host: '0.0.0.0',
+    strictPort: true,
+    port: 5173,
+    watch: {
+      usePolling: true
     },
+    hmr: {
+      host: 'casio-laravel.onrender.com'
+    }
+  },
+  preview: {
+    allowedHosts: ['casio-laravel.onrender.com']
+  },
+  // plugins ...
 });
